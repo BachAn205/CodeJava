a8c890235613bfe369945c386451780e035fc95b
BachAn <anbk27122005@gmail.com>
2024-09-17 07:25:20 +0700

    code

---
+++ Learn.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.learn;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author khanh
+ */
+public class Learn {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Bach Khanh An");
+        System.out.println("23IT4");
+        
+       Scanner s = new Scanner(System.in);
+         System.out.println("Nhap ten sinh vien: " );
+       String name = s.nextLine();
+        System.out.println("Ten sinh vien la: " + name);
+      
+       
+    }
+}
